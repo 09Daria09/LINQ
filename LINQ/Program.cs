75292dfb06741f4e3065f98d1b2aa45dcf625631
Daria <dashakukuruza86@gmail.com>
2023-03-07 12:44:50 +0200

    all

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -26,6 +26,7 @@ namespace LINQ
             public string FullNameDirector { get; set; }
             public int NumberEmployees { get; set; }
             public string Address { get; set; } 
+            public List<Employee> Employees { get; set; }
             public Firm() 
             {
                 Name = null;
@@ -34,6 +35,7 @@ namespace LINQ
                 FullNameDirector = null;
                 NumberEmployees = 0;
                 Address = null;
+                Employees = new List<Employee>();
             }
             public Firm(string name, int foundingDate, string fullNameDirector, Profile profile, int numberEmployees, string address) 
             {
@@ -43,6 +45,10 @@ namespace LINQ
                 FullNameDirector = fullNameDirector;
                 NumberEmployees = numberEmployees;
                 Address = address;
+            }
+            public Firm(string name, int foundingDate, string fullNameDirector, Profile profile, int numberEmployees, string address, List<Employee> employees): this(name, foundingDate, fullNameDirector, profile, numberEmployees, address)
+            {
+                employees = new List<Employee>();
             }
             public override string ToString()
             {
@@ -54,20 +60,49 @@ namespace LINQ
             public string FullName { get; set; }
             public string Post { get; set; }
             public string Phone { get; set; }
+            public string Email { get; set; }
+            public double Wage { get; set; }
+
+            public Employee(string fullName, string post, string phone, string email, double wage)
+            {
+                FullName = fullName;
+                Post = post;
+                Phone = phone;
+                Email = email;
+                Wage = wage;
+            }
+
+            public Employee() { }
+            public override string ToString()
+            {
+                return $"Name: {FullName}, Post: {Post}, Phone: {Phone}, Email: {Email}, Wage: {Wage}";
+            }
         }
 
         static void Main(string[] args)
         {
-            Firm firm1 = new Firm("ABC Company", 1990, "John Smith", Profile.IT, 100, "123 Main St");
-            Firm firm2 = new Firm("XYZ Inc", 2005, "Jane Doe", Profile.Marketing, 50, "456 Broadway");
-            Firm firm3 = new Firm("Food Corporation", 1980, "Bob Johnson", Profile.Restaurant, 200, "789 Oak Ave");
-            Firm firm4 = new Firm("Big Co", 2023, "Samantha Lee", Profile.Construction, 75, "321 Elm St");
-            Firm firm5 = new Firm("White Enterprises", 1995, "Mike Black", Profile.Trading, 150, "555 Pine St");
-            Firm firm6 = new Firm("Superior Industries", 1975, "Karen Jones", Profile.IT, 300, "222 Maple Ave");
-            Firm firm7 = new Firm("Elite Solutions", 2000, "David Brown", Profile.Marketing, 25, "777 Main St");
-            Firm firm8 = new Firm("Pinnacle Group", 2022, "Anna Kim", Profile.Construction, 50, "444 Market St");
-            Firm firm9 = new Firm("Innovative Ventures", 1998, "Chris White", Profile.Trading, 125, "999 Broadway");
-            Firm firm10 = new Firm("Advanced Technologies", 1985, "Emily Chen", Profile.IT, 175, "333 Oak Ave");
+            Employee employee1 = new Employee("Lionel Smith", "Manager", "555-1234", "kris@email.com", 15000);
+            Employee employee2 = new Employee("Joy Gofri", "Assistant Manager", "555-5678", "joy@email.com", 29000);
+            Employee employee3 = new Employee("Bob Johnson", "Sales Representative", "555-9012", "diohnson@email.com", 35000);
+            Employee employee4 = new Employee("Samantha Lee", "Marketing Specialist", "555-3456", "slee@email.com", 30000);
+            Employee employee5 = new Employee("Mike Davis", "IT Technician", "555-7890", "mdavis@email.com", 31000);
+            Employee employee6 = new Employee("Karen Jones", "Human Resources Manager", "555-2345", "kjones@email.com", 55000);
+            Employee employee7 = new Employee("David Brown", "Accountant", "555-6789", "dbrown@email.com", 50000);
+            Employee employee8 = new Employee("Anna Kim", "Office Assistant", "555-0123", "akim@email.com", 30000);
+            Employee employee9 = new Employee("Chris Lee", "Programmer", "555-4567", "clee@email.com", 55000);
+            Employee employee10 = new Employee("Emily Chen", "Graphic Designer", "555-8901", "echen@email.com", 45000);
+            List<Employee> employee = new List<Employee> { employee1, employee2, employee3, employee4, employee5, employee6, employee7, employee8, employee9, employee10 };
+
+            Firm firm1 = new Firm("ABC Company", 1990, "John Smith", Profile.IT, 100, "123 Main St", employee);
+            Firm firm2 = new Firm("XYZ Inc", 2005, "Jane Doe", Profile.Marketing, 50, "456 Broadway", employee);
+            Firm firm3 = new Firm("Food Corporation", 1980, "Bob Johnson", Profile.Restaurant, 200, "789 Oak Ave", employee);
+            Firm firm4 = new Firm("Big Co", 2023, "Samantha Lee", Profile.Construction, 75, "321 Elm St", employee);
+            Firm firm5 = new Firm("White Enterprises", 1995, "Mike Black", Profile.Trading, 150, "555 Pine St", employee);
+            Firm firm6 = new Firm("Superior Industries", 1975, "Karen Jones", Profile.IT, 300, "222 Maple Ave", employee);
+            Firm firm7 = new Firm("Elite Solutions", 2000, "David Brown", Profile.Marketing, 25, "777 Main St", employee);
+            Firm firm8 = new Firm("Pinnacle Group", 2022, "Anna Kim", Profile.Construction, 50, "444 Market St", employee);
+            Firm firm9 = new Firm("Innovative Ventures", 1998, "Chris White", Profile.Trading, 125, "999 Broadway", employee);
+            Firm firm10 = new Firm("Advanced Technologies", 1985, "Emily Chen", Profile.IT, 175, "333 Oak Ave", employee);
             List<Firm> firms = new List<Firm> { firm1, firm2, firm3, firm4, firm5, firm6, firm7, firm8, firm9, firm10 };
 
             #region 1
@@ -137,6 +172,76 @@ namespace LINQ
             //}
             #endregion
 
+            #region 2
+            //Получить всех сотрудников конкретной фирмы
+            var employeesInFirm = from e in employee
+                                  from f in firms
+                                  where f.Name == "ABC Company"
+                                       select e;
+
+            //foreach (Employee info in employeesInFirm)
+            //{
+            //    Console.WriteLine(info);
+            //}
+
+            //Получить всех сотрудников конкретной фирмы, у которых заработные платы больше заданной
+            var employeesWage = from e in employee
+                                  from f in firms
+                                  where f.Name == "ABC Company"
+                                  where e.Wage > 30000 
+                                  select e;
+
+            //foreach (Employee info in employeesWage)
+            //{
+            //    Console.WriteLine(info);
+            //}
+
+            //Получить сотрудников всех фирм, у которых должность менеджер
+            var employeesManager = from e in employee
+                                from f in firms
+                                where e.Post == "Manager"
+                                select e;
+            //foreach (Employee info in employeesManager)
+            //{
+            //    Console.WriteLine(info);
+            //}
+
+            //Получить сотрудников, у которых телефон начинается с 23
+            var employeesPhone = from e in employee
+                                from f in firms
+                                where f.Name == "ABC Company"
+                                where e.Phone.StartsWith("23")
+                                select e;
+
+            //foreach (Employee info in employeesPhone)
+            //{
+            //    Console.WriteLine(info);
+            //}
+
+            //Получить сотрудников, у которых Email начинается с di
+            var employeesEmail = from e in employee
+                                 from f in firms
+                                 where f.Name == "ABC Company"
+                                 where e.Email.StartsWith("di")
+                                 select e;
+
+            //foreach (Employee info in employeesEmail)
+            //{
+            //    Console.WriteLine(info);
+            //}
+
+            //Получить сотрудников, у которых имя Lione
+            var employeesName = from e in employee
+                                 from f in firms
+                                 where f.Name == "ABC Company"
+                                 where e.FullName.Contains("Lionel")
+                                select e;
+
+            foreach (Employee info in employeesName)
+            {
+                Console.WriteLine(info);
+            }
+            #endregion
         }
     }
 }
